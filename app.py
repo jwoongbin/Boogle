791f2a2c1f7060b5e7fbf06b1bd2561b8ed17824
DESKTOP-RHQTACD\User <philoleben@gmail.com>
2022-04-29 22:47:14 +0900

    Main / Result 템플릿 분리

--- app.py
+++ app.py
@@ -11,19 +11,9 @@ def home():
         response = requests.get("http://gutendex.com/books/?search="+search_word)
         data = json.loads(response.content)
         books = data['results']
-    return render_template('main.html', books=books)
-
-
-
-@app.route('/results')
-def results():
-    books = []
-    if 'book' in request.args:
-        search_word = request.args.get('book')
-        response = requests.get("http://gutendex.com/books/?search="+search_word)
-        data = json.loads(response.content)
-        books = data['results']
-    return render_template('results.html', books=books)
+        return render_template('results.html', books=books)
+    else:
+        return render_template('main.html', books=books)
 
 app.run(debug= True)
 
